ab677604d6a17b3fc657c5f55aa1024ef667843d
Nikola Shopov <>
2022-06-01 20:25:04 +0300

    add watchers and subscribers to repository screen

--- GitHubExplorer/GitHubExplorer/Scenes/Repositories/Repository/RepositoryViewController.swift
+++ GitHubExplorer/GitHubExplorer/Scenes/Repositories/Repository/RepositoryViewController.swift
@@ -11,15 +11,18 @@ import UIKit
 public class RepositoryViewController: UIViewController {
     public var repositoryData: RepositoryDTO?
     
-    @IBOutlet weak var lblStarred: UILabel!
     @IBOutlet weak var txtFull: UITextView!
     @IBOutlet weak var lblName: UILabel!
     
-    @IBOutlet weak var lblForks: UILabel!
+    @IBOutlet weak var lblWatchers: UILabel!
+    @IBOutlet weak var lblSubscribers: UILabel!
     
     public override func viewDidLoad() {
         if let repositoryData = repositoryData {
             lblName.text = repositoryData.name
+            txtFull.text = repositoryData.fullName
+            lblWatchers.text = "\(repositoryData.watchersCount ?? 0) Watchers"
+            lblSubscribers.text = "\(repositoryData.subscribersCount ?? 0) Subscribers"
         }
     }
 }
